1e35b331aed5ee8975cd59e5b4d5630a9c338506
Preksha Dahal <prekshadahal@Prekshas-MacBook-Pro.local>
2022-08-16 11:12:25 +0545

    initial commi

---
+++ functions.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+//Shape 1
+
+var length = 5
+var width = 10
+
+var area = length * width
+
+var length2 = 6
+var width2 = 12
+
+let area2 = length2 * width2
+print (area2)
+
+/*
+ A process of p
+ */
